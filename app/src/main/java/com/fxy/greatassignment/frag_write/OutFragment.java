23b0b1fafaab11c3e813d7c59f01f0534fcf22dd
Arete <KakiaArete0@163.com>
2022-06-18 18:17:37 +0800

    outfrag 正常

--- app/src/main/java/com/fxy/greatassignment/frag_write/OutFragment.java
+++ app/src/main/java/com/fxy/greatassignment/frag_write/OutFragment.java
@@ -3,22 +3,29 @@ package com.fxy.greatassignment.frag_write;
 import android.inputmethodservice.KeyboardView;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.fxy.greatassignment.R;
+import com.fxy.greatassignment.database.AccountBean;
 import com.fxy.greatassignment.database.DBManager;
 import com.fxy.greatassignment.database.TypeBean;
 import com.fxy.greatassignment.utils.KeyBoardUtils;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -32,8 +39,20 @@ public class OutFragment extends Fragment {
     ImageView imageView;
     TextView type,remark,time;
     GridView gridView;
+    // 创建Bean List
     List<TypeBean> typeBeanList;
     TypeBaseAdapter adapter;
+    // 创建记账数据对象
+    AccountBean accountBean;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        accountBean = new AccountBean();   //创建对象
+        accountBean.setTypename("其他");
+        accountBean.setsImageId(R.mipmap.ic_qita_hs);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,9 +61,47 @@ public class OutFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_out,container,false);
         //初始化view
         initView(view);
+        //获取当前时间显示到右下角
+        setInitTime();
         // 给GridView填充数据
         loadDataToGV();
+        setGVListener(); //设置GridView每一项的点击事件
         return view;
+    }
+
+    private void setInitTime() {
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日 HH:mm");
+        String time_now = sdf.format(date);
+        time.setText(time_now);
+        accountBean.setTime(time_now);
+
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH)+1;
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        accountBean.setYear(year);
+        accountBean.setMonth(month);
+        accountBean.setDay(day);
+    }
+
+    private void setGVListener() {
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                adapter.selectPos = position;
+                adapter.notifyDataSetInvalidated();  //提示绘制发生变化了
+
+                TypeBean typeBean = typeBeanList.get(position);
+                String typename = typeBean.getTypename();
+                type.setText(typename);
+                accountBean.setTypename(typename);
+
+                int simageId = typeBean.getSimageId();
+                imageView.setImageResource(simageId);
+                accountBean.setsImageId(simageId);
+            }
+        });
     }
 
 
@@ -78,8 +135,16 @@ public class OutFragment extends Fragment {
             @Override
             public void onEnsure() {
                 // TODO 点击确定按钮
+                String moneyStr = editText.getText().toString();
+                if (TextUtils.isEmpty(moneyStr)||moneyStr.equals("0")) {
+                    getActivity().finish();
+                    return;
+                }
+                float money = Float.parseFloat(moneyStr);
+                accountBean.setMoney(money);
                 // TODO 记录当前edittext信息，保存到数据库中
                 // TODO 返回上级页面
+                getActivity().finish();
             }
         });
     }
